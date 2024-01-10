a507fe88fe256b064ef252e2395192957ae148fb
Samuel Shapley <samuelshapley@gmail.com>
2024-01-10 13:04:41 +0000

    image urls

--- examples/openai-launchpad/image-generation-and-description.py
+++ examples/openai-launchpad/image-generation-and-description.py
@@ -6,8 +6,8 @@ model = "gpt-3.5-turbo-1106"
 
 images = Images()
 
-image_generation = images.generate_image("What the universe looks like to an outside observer.", fix_prompt=False, display_image=True, save_image=True)
+image_generation = images.generate_image("What the universe looks like to an outside observer.", fix_prompt=False, response_format="url", display_image=True, save_image=True)
 
 vision = Vision(system="You can only respond with bullet points.")
 
-vision.vision_completion(prompt="What is this image showing?", image_paths=[image_generation["path_to_image"]], memories=False, stream=True)
+vision.vision_completion(prompt="What is this image showing?", image_urls=[image_generation["url"]], memories=False, stream=True)
